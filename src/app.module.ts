94b0e41dc8648f188cbe33b2c2881339f608d6ef
yousef <yousef.rezaei9@gmail.com>
2025-09-09 14:21:01 +0200

    Add a logger middleware to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
   imports: [UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -1,10 +1,22 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
   use(req: Request, res: Response, next: () => void) {
-    req.
+    const { ip, method, baseUrl } = req;
+    const userAgent = req.get('user-agent') || '';
+    const startAt = process.hrtime();
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const constLength = res.get('content-length');
+      const dif = process.hrtime(startAt);
+      const responseTime = dif[0] * 1e3 + dif[1] * 1e-6;
+      this.logger.log(
+        `${method} ${baseUrl} ${statusCode} ${constLength} - ${userAgent} ${ip} ${responseTime.toFixed(2)}ms`,
+      );
+    });
     next();
   }
 }
